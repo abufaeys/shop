259834d19b2163605ff0382b1ffe69b3ade656fb
mohammad shalhoob <abufaeys@gmail.com>
2022-05-26 23:30:07 +0300

    Initial Commit

---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by mohammad shalhoob on 26/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
